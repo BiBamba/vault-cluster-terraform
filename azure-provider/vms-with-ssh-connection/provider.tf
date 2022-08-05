2a42dc720d1016f8a8e7d4e47b40140a93c88a6c
Bi Bamba <maxime-tizie@hotmail.fr>
2022-08-05 16:22:29 -0500

    Add tf config for vm with ssh connection creation

---
+++ azure-provider/vms-with-ssh-connection/provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+  features {}
+  client_id       = "<appid_value>"
+  client_secret   = "<password_value>"
+  tenant_id       = "<tenant_id_value>"
+  subscription_id = "<subscription_id_value>"
+}
